a064397eef8976248c46f8d79a4b55a6fdb447cc
guillegda <guigardelal@gmail.com>
2025-08-16 12:15:53 +0200

    avances de asignacion de parametros

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# main.py
 from interface import InterfaceApp
 
 if __name__ == "__main__":
     app = InterfaceApp()
+    # Genera una ventana de texto
+    # Esta ventana permite al usuario introducir un texto y que la API devuelva un texto formato json
     app.mainloop()
